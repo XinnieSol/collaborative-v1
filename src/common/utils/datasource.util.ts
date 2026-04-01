1a4e7c74fd10300d30c86b9eb6b7732f6531e8a5
ZeeZinnie <ezinne.ezeme@gmail.com>
2026-04-01 21:48:09 +0100

    Moified datasource to suite render configs

--- src/common/utils/datasource.util.ts
+++ src/common/utils/datasource.util.ts
@@ -3,19 +3,19 @@ import { SnakeCaseNamingStrategy } from 'src/common/utils/snake-case-naming-stra
 import * as dotenv from 'dotenv';
 import { DataSource } from 'typeorm';
 import { PostgresConnectionOptions } from 'typeorm/driver/postgres/PostgresConnectionOptions';
-import path from 'path';
 
 dotenv.config();
 
-const databaseConfig = getConfig();
+export const getDefaultDataSource = () => {
+    dotenv.config();
 
-const isProd = process.env.NODE_ENV === 'production';
+    const databaseConfig = getConfig();
+    const isProd = process.env.NODE_ENV === 'production';
 
-export const AppDataSource = new DataSource({
-    ...databaseConfig,
-    entities: isProd ? ['dist/**/*.entity.js'] : ['src/**/*.entity.ts'],
-    migrations: isProd ? ['dist/migrations/*.js'] : ['src/migrations/*.ts'],
-    namingStrategy: new SnakeCaseNamingStrategy(),
-} as PostgresConnectionOptions);
-
-export default AppDataSource;
+    return new DataSource({
+        ...databaseConfig,
+        entities: isProd ? ['dist/**/*.entity.js'] : ['src/**/*.entity.ts'],
+        migrations: isProd ? ['dist/migrations/*.js'] : ['src/migrations/*.ts'],
+        namingStrategy: new SnakeCaseNamingStrategy(),
+    } as PostgresConnectionOptions);
+};
